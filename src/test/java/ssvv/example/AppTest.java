0e5f0315251ef23768fb940996d52464d0808ab2
Razvan <razvan99h@yahoo.com>
2021-03-09 17:52:58 +0200

    TCE1 added

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,8 +1,5 @@
 package ssvv.example;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import Domain.Student;
 import Exceptions.ValidatorException;
 import Repository.XMLFileRepository.StudentXMLRepo;
@@ -10,6 +7,8 @@ import Service.TxtFileService.StudentService;
 import Service.XMLFileService.StudentXMLService;
 import Validator.StudentValidator;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 
 public class AppTest {
@@ -29,5 +28,16 @@ public class AppTest {
         assertEquals(student1.getGrupa(), Integer.parseInt(grupa));
         assertEquals(student1.getEmail(), email);
         assertEquals(student1.getIndrumator(), prof);
+    }
+
+    @Test
+    public void TCE1() {
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo studentXMLRepo = new StudentXMLRepo(studentValidator, "StudentiXML.xml");
+        studentXMLService = new StudentXMLService(studentXMLRepo);
+
+        String id = "1", nume = "name1", grupa = "invalid", email = "email1@gmail.com", prof = "prof1";
+        String[] params = {id, nume, grupa, email, prof};
+        ValidatorException e = assertThrows(ValidatorException.class, () -> studentXMLService.add(params));
     }
 }
