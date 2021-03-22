f3032ef97a817cfae0358914e4bc2883423fb95c
Razvan <razvan99h@yahoo.com>
2021-03-23 01:49:01 +0200

    Take home lab 2 - second round of tests - all passing

--- src/main/java/Service/XMLFileService/AbstractXMLService.java
+++ src/main/java/Service/XMLFileService/AbstractXMLService.java
@@ -1,5 +1,6 @@
 package Service.XMLFileService;
 import Domain.*;
+import Exceptions.ServiceException;
 import Exceptions.ValidatorException;
 import Repository.XMLFileRepository.AbstractXMLRepo;
 
@@ -10,18 +11,20 @@ public abstract class AbstractXMLService<ID,E extends HasId<ID>> {
         this.xmlrepo = xmlrepo;
     }
 
-    protected abstract E extractEntity(String[] params);
+    protected abstract E extractEntity(String[] params) throws ValidatorException;
         //return new Student(params[0],params[1],Integer.parseInt(params[2]),params[3],params[4]);
     //}
 
     public void add(String params[]) throws ValidatorException {
         E e=extractEntity(params);
+        if (findOne(e.getId()) != null)
+            throw new ServiceException("Already exists!");
         xmlrepo.save(e);
     }
     public void remove(ID id){
         xmlrepo.delete(id);
     }
-    public void update(String params[]){
+    public void update(String params[]) throws ValidatorException {
         E s=extractEntity(params);
         xmlrepo.update(s);
     }
--- src/main/java/Service/XMLFileService/StudentXMLService.java
+++ src/main/java/Service/XMLFileService/StudentXMLService.java
@@ -1,7 +1,11 @@
 package Service.XMLFileService;
 
 import Domain.Student;
+import Exceptions.ValidatorException;
 import Repository.XMLFileRepository.StudentXMLRepo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StudentXMLService extends AbstractXMLService<String,Student>{
     private StudentXMLRepo xmlrepo;
@@ -11,10 +15,17 @@ public class StudentXMLService extends AbstractXMLService<String,Student>{
     }
 
     @Override
-    protected Student extractEntity(String[] params){
+    protected Student extractEntity(String[] params) throws ValidatorException {
+        final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+
         int grupa=0;
         try{
             grupa=Integer.parseInt(params[2]);
+            if (grupa < 100 || grupa > 999)
+                throw new ValidatorException("Invalid group!");
+            Matcher matcher = Pattern.compile(EMAIL_REGEX).matcher(params[3]);
+            if (!matcher.matches())
+                throw new ValidatorException("Invalid email!");
         }catch(NumberFormatException ex){
             System.out.println(ex.getMessage());
         }
